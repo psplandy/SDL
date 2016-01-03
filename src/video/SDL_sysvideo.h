9ce55091be1347d2fcc462aca2725e817c245bb6
Jools Wills <buzz@exotica.org.uk>
2016-01-03 21:27:57 +0000

    mali-fbdev support
      * mali-fbdev support from https://github.com/mihailescu2m/libsdl2-2.0.2-dfsg1 with changes for newer SDL

--- src/video/SDL_sysvideo.h
+++ src/video/SDL_sysvideo.h
@@ -438,6 +438,7 @@ extern VideoBootStrap VITA_bootstrap;
 extern VideoBootStrap RPI_bootstrap;
 extern VideoBootStrap KMSDRM_bootstrap;
 extern VideoBootStrap KMSDRM_LEGACY_bootstrap;
+extern VideoBootStrap MALI_bootstrap;
 extern VideoBootStrap DUMMY_bootstrap;
 extern VideoBootStrap Wayland_bootstrap;
 extern VideoBootStrap NACL_bootstrap;
